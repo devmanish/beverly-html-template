a4c478002295b0e825ae28308aa080964c7799ca
web-ram <webgrity70@gmail.com>
2021-12-30 18:42:19 +0530

    mmenu added

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,41 @@
-/* custom js */
+/* custom js
+
+    var height = $('.topHeader').outerHeight();
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > height)
+        {
+            $(".header").addClass("fixedHeader");
+            
+        }
+        else if($(this).scrollTop() <= height)
+        {
+             $(".header").removeClass("fixedHeader");
+            
+        }     
+    });
+
+
+    var menu = new MmenuLight(
+        document.querySelector( '#navbarSupportedContent' ),
+        '(max-width: 991px)'
+    );
+
+    var navigator = menu.navigation({
+            selectedClass: 'Selected',
+            slidingSubmenus: true,
+            theme: 'dark',
+            title: 'Menu'
+    });
+
+    var drawer = menu.offcanvas({
+         position: 'left'
+    });
+
+
+    document.querySelector( '.navbar-toggler' ).addEventListener( 'click', evnt => {
+        evnt.preventDefault();
+        drawer.open();
+    });
+
+ */
